c9f397aae64b6a10f2e5a5234927c334a18e6122
suvojit mondal <msuvojit@gmail.com>
2017-09-04 21:17:11 +0530

    add all files

---
+++ data.py
@@ -0,0 +1,33 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit condimentum ultrices. Duis aliquam ornare est id laoreet. Integer aliquam nisi mollis neque dapibus, in consequat nunc pulvinar. Mauris sed euismod massa. Nam sollicitudin hendrerit velit, ac tincidunt turpis congue at.',
+            'author': 'Suvojit Mondal',
+            'create_date': '15-06-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit condimentum ultrices. Duis aliquam ornare est id laoreet. Integer aliquam nisi mollis neque dapibus, in consequat nunc pulvinar. Mauris sed euismod massa. Nam sollicitudin hendrerit velit, ac tincidunt turpis congue at.',
+            'author': 'Suvojit Mondal',
+            'create_date': '15-06-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit condimentum ultrices. Duis aliquam ornare est id laoreet. Integer aliquam nisi mollis neque dapibus, in consequat nunc pulvinar. Mauris sed euismod massa. Nam sollicitudin hendrerit velit, ac tincidunt turpis congue at.',
+            'author': 'Suvojit Mondal',
+            'create_date': '15-06-2017'
+        },
+        {
+            'id': 4,
+            'title': 'Article Four',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit condimentum ultrices. Duis aliquam ornare est id laoreet. Integer aliquam nisi mollis neque dapibus, in consequat nunc pulvinar. Mauris sed euismod massa. Nam sollicitudin hendrerit velit, ac tincidunt turpis congue at.',
+            'author': 'Suvojit Mondal',
+            'create_date': '15-06-2017'
+        },
+    ]
+
+    return articles
